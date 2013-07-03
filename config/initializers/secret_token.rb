6e29c47db8f47cd7987b2b92a45545b395ca671d
Albert <guozhaonan@gmail.com>
2013-07-03 01:42:09 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lothanh::Application.config.secret_token = '2d0cc733400ed55d7a0a396df30b36fee027ad66f103c2b306fb5fe93cf559b0619f72d2f37dc8268f0de762b3604d7d758e4753ad14612c646944317e54a638'
